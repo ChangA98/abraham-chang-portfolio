ea2c725bc643a02cbc94485ba50343c4ff6e840b
Abraham Chang <soyabrahamc@gmail.com>
2020-09-23 20:11:50 -0400

    fix img alt

--- src/components/sections/Project.js
+++ src/components/sections/Project.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Img from 'gatsby-image';
 
 const Project = ({
     id,
@@ -13,7 +12,7 @@ const Project = ({
             <p>{description}</p>
             <div>
                 {images.map(({ url, title }) => (
-                    <img style={{ width: 100, marginRight: 20 }} src={url} title={title || 'Prject Image'} />
+                    <img style={{ width: 100, marginRight: 20 }} src={url} alt={title || 'Prject Image'} />
                 ))}
             </div>
         </section>
